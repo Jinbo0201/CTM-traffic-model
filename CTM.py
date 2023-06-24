ac15d65212601f75acc849e4e54e90f8099e60c5
Jinbo0201 <jinbo0201@outlook.com>
2023-06-24 17:02:16 +0800

    change CTM.py

--- CTM.py
+++ CTM.py
@@ -174,31 +174,31 @@ class CTM(object):
 
             
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    ctm = CTM()
+#     ctm = CTM()
     
-    ctm.start()
+#     ctm.start()
     
-    idOnRamp = 7
+#     idOnRamp = 7
     
-    step = 0    
-    while step < 360:        
-        ctm.simulationStep()
+#     step = 0    
+#     while step < 360:        
+#         ctm.simulationStep()
         
-        if step % 15 == 0 :        
-            ctm.changeDemandZero(5000 + random.randint(-500, 500))
-            ctm.changeDemandOnRamp(2000 + random.randint(-500, 500), idOnRamp)
+#         if step % 15 == 0 :        
+#             ctm.changeDemandZero(5000 + random.randint(-500, 500))
+#             ctm.changeDemandOnRamp(2000 + random.randint(-500, 500), idOnRamp)
               
-        if step % 15 == 0 :            
-            ctm.changeRatioOnRamp(random.choice([0.6, 0.7, 0.8, 0.9, 1]), idOnRamp)
+#         if step % 15 == 0 :            
+#             ctm.changeRatioOnRamp(random.choice([0.6, 0.7, 0.8, 0.9, 1]), idOnRamp)
         
-        # ctm.saveData()
-        step = step + 1
+#         # ctm.saveData()
+#         step = step + 1
         
-    ctm.close()
+#     ctm.close()
     
-    ctm.show()
+#     ctm.show()
     
     
     
